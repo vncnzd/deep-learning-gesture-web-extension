f97dbfc113e5c8e467ebad9de35f8fce1225d173
Vincenz Dziedzina <vincenz.dziedzina@gmail.com>
2020-03-23 21:18:56 +0100

    Add packages for sass/css loading and extraction for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,4 +7,19 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [new MiniCssExtractPlugin()],
+    module: {
+        rules: [
+          {
+            test: /\.s[ac]ss$/i,
+            use: [
+                MiniCssExtractPlugin.loader,
+                // Translates CSS into CommonJS
+                'css-loader',
+                // Compiles Sass to CSS
+                'sass-loader',
+            ],
+          },
+        ],
+      },
 };
