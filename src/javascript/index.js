48576fc5dcd022460768e117f9883edce57771d6
Vincenz Dziedzina <vincenz.dziedzina@gmail.com>
2020-03-28 20:34:14 +0100

    Add execution of browser commands according to prediction result and loading of stored models

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -12,16 +12,17 @@ class App {
         this.imagePreviewCanvasElement = document.querySelector('#image-preview-canvas');
         this.featureSelectElement = document.querySelector('#feature-select');
         this.examplesSpanElement = document.querySelector("#examples-span");
-        this.predictButtonElement = document.querySelector("#predict-button");
         this.numberOfEpochsInputElement = document.querySelector("#number-of-epochs-input");
         this.loadingBarElement = document.querySelector("#training-loading-bar");
         this.lossSpanElement = document.querySelector("#loss-span");
+        this.activationThreshold = 0.9;
 
         this.numberOfExamples = 0;
         this.loadingBarProgress = 0;
         this.webcam = new Webcam(this.videoElement, 50);
         this.data = new DataContainer();
-        this.model = new Model(50, 50, 3, 4);
+        this.model = new Model(50, 50, 3, 4, 'localstorage://gesture-extension-model');
+        this.model.load(this.model.storageDirectory);
 
         this.addEventListeners();
     }
@@ -39,19 +40,10 @@ class App {
             this.examplesSpanElement.innerHTML = ++this.numberOfExamples;
         });
 
-        this.trainNetworkButtonElement.addEventListener('click', () => {
+        this.trainNetworkButtonElement.addEventListener('click', async () => {
             let numberOfEpochs = parseInt(this.numberOfEpochsInputElement.value);
-            this.model.train(this.data.xTrain, this.data.yTrain, numberOfEpochs, this.updateTrainingProgress.bind(this));
-        });
-
-        this.predictButtonElement.addEventListener('click', () => {
-            let imageTensor = this.webcam.captureImageAndGetTensor();
-            let resultTensor = tf.tidy(() => { return this.model.predict(imageTensor.expandDims(0)) });
-
-            resultTensor.print();
-
-            tf.dispose(imageTensor);
-            tf.dispose(resultTensor);
+            await this.model.train(this.data.xTrain, this.data.yTrain, numberOfEpochs, this.updateTrainingProgress.bind(this));
+            this.makePredictionEverySeconds(2)
         });
     }
 
@@ -64,7 +56,59 @@ class App {
         this.loadingBarProgress += progressStep;
         this.loadingBarElement.style.width = this.loadingBarProgress + "%";
     }
+
+    makePredictionEverySeconds(numberOfSeconds) {
+        let milliseconds = numberOfSeconds * 1000;
+        window.setInterval(this.makePrediction.bind(this), milliseconds);
+    }
+
+    makePrediction() {
+        let imageTensor = this.webcam.captureImageAndGetTensor();
+        let resultTensor = tf.tidy(() => { return this.model.predict(imageTensor.expandDims(0)) });
+
+        this.evaluatePredictionTensor(resultTensor);
+
+        tf.dispose(imageTensor);
+        tf.dispose(resultTensor);
+    }
+
+    evaluatePredictionTensor(predictionTensor) {
+        let maxPredictionValue = predictionTensor.max().dataSync()[0];
+        console.log("Max prediction value: " + maxPredictionValue);
+
+        if (maxPredictionValue < this.activationThreshold) return;
+
+        let label = predictionTensor.argMax(1).dataSync()[0];
+        console.log("label: " + label);
+
+        this.executeAction(label);
+    }
+
+    executeAction(label) {
+        switch (label) {
+            case 1:
+                browser.tabs.create({
+                    active: true
+                });
+                break;
+            case 2:
+                browser.tabs.query({currentWindow: true, active: true}).then(function(tabs) {
+                    if (tabs.length > 0) {
+                        browser.tabs.remove(tabs[0].id);
+                    }
+                });
+                break;
+            case 3:
+                browser.tabs.query({currentWindow: true, active: true}).then(function(tabs) {
+                    if (tabs.length > 0) {
+                        browser.tabs.reload(tabs[0].id);
+                    }
+                });
+                break;
+            default:
+                break;
+        }
+    }
 }
 
 let app = new App();
-
